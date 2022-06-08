dc2d29dbbef053b8457029531ec76f90eaebe77f
Faruk <39664360+farkmu45@users.noreply.github.com>
2022-06-08 21:19:47 +0700

    Update create.blade.php

--- resources/views/pages/dashboard/product/create.blade.php
+++ resources/views/pages/dashboard/product/create.blade.php
@@ -67,7 +67,7 @@
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                                 Stock Product
                             </label>
-                            <input value="{{ old('stock_product') }}" name="name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Stock_Product">
+                            <input value="{{ old('stock_product') }}" name="stock_product" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Stock_Product">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
